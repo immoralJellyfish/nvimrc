153902fe87e6a77359cca7c1610c8e70f2b32594
akbarsanihasan <akbarsani.dev@gmail.com>
2024-10-08 09:41:28 +0700

    update: Add trash-cli integration

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -51,5 +51,39 @@ return {
 			enabled = true,
 			leave_dirs_open = false,
 		},
+		filesystem = {
+			commands = {
+				delete = function(state)
+					local inputs = require("neo-tree.ui.inputs")
+					local path = state.tree:get_node().path
+					local msg = "Are you sure you want to trash " .. path
+
+					inputs.confirm(msg, function(confirmed)
+						if not confirmed then
+							return
+						end
+
+						vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+						require("neo-tree.sources.manager").refresh(state.name)
+					end)
+				end,
+
+				delete_visual = function(state, selected_nodes)
+					local inputs = require("neo-tree.ui.inputs")
+					local msg = "Are you sure you want to trash " .. #selected_nodes .. " files ?"
+
+					inputs.confirm(msg, function(confirmed)
+						if not confirmed then
+							return
+						end
+
+						for _, node in ipairs(selected_nodes) do
+							vim.fn.system({ "trash", vim.fn.fnameescape(node.path) })
+						end
+						require("neo-tree.sources.manager").refresh(state.name)
+					end)
+				end,
+			},
+		},
 	},
 }
