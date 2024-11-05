4aaa117a71ede3554d161754018cd6fd364e8945
akbarsanihasan <akbarsani.dev@gmail.com>
2024-11-05 10:00:53 +0700

    change: hide lualine lazy

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,8 +8,8 @@ return {
 			component_separators = "",
 			section_separators = "",
 			disabled_filetypes = {
-				statusline = { "undotree", "fugitive", "mason", "diff", "TelescopePrompt", "neo-tree" },
-				winbar = { "undotree", "fugitive", "mason", "diff", "TelescopePrompt", "neo-tree" },
+				statusline = { "lazy", "undotree", "fugitive", "mason", "diff", "TelescopePrompt", "neo-tree" },
+				winbar = { "lazy", "undotree", "fugitive", "mason", "diff", "TelescopePrompt", "neo-tree" },
 			},
 			ignore_focus = { "TelescopePrompt", "neo-tree", "toggleterm" },
 			extensions = { "lazy", "nvim-tree", "toggleterm" },
