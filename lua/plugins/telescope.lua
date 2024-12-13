eb9f50cdea0b92ce7b610897bc1170958b38ef8c
akbarsanihasan <akbarsani.dev@gmail.com>
2024-12-13 22:16:10 +0700

    change: telescope keybind

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,27 +7,40 @@ return {
 	keys = function()
 		local builtin = require("telescope.builtin")
 		return {
-			{ "<C-p>", builtin.find_files, silent = true, noremap = true },
-			{ "<leader>pg", builtin.git_files, silent = true, noremap = true },
-			{ "<leader>ps", builtin.live_grep, silent = true, noremap = true },
-			{ "<leader>vh", builtin.help_tags, silent = true, noremap = true },
 			{
-				"<leader>pws",
+				"<C-p>",
 				function()
-					local word = vim.fn.expand("<cword>")
-					builtin.grep_string({ search = word })
+					builtin.find_files({ hidden = true })
 				end,
 				silent = true,
 				noremap = true,
 			},
-
+			{
+				"<leader>pws",
+				function()
+					local word = vim.fn.expand("<cword>")
+					builtin.grep_string({ search = word, hidden = true })
+				end,
+				silent = true,
+				noremap = true,
+			},
 			{
 				"<leader>pWs",
 				function()
 					local word = vim.fn.expand("<cWORD>")
-					builtin.grep_string({ search = word })
+					builtin.grep_string({ search = word, hidden = true })
 				end,
 			},
+			{
+				"<leader>pg",
+				function()
+					builtin.git_files({ hidden = true })
+				end,
+				silent = true,
+				noremap = true,
+			},
+			{ "<leader>ps", builtin.live_grep, silent = true, noremap = true },
+			{ "<leader>vh", builtin.help_tags, silent = true, noremap = true },
 		}
 	end,
 }
