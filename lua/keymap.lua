aa50ef4caa97eef23336bee6ed9b4d2823620588
immoralJellyfish <akbarsani.dev@gmail.com>
2024-12-18 07:16:33 +0000

    change: kms

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -12,7 +12,10 @@ vim.keymap.set("n", "J", "mzJ`z", opts)
 vim.keymap.set("n", "n", "nzzzv", opts)
 vim.keymap.set("n", "N", "Nzzzv", opts)
 
--- Greatest remap ever
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set("v", "<leader>p", '"_dP', opts)
