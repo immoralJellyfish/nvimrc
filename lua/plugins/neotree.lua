2263459a8a591b3f9e3889cca9ef8fd48e9b3b43
akbarsanihasan <akbarsani.dev@gmail.com>
2025-01-24 09:01:41 +0700

    change: move function

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,34 +1,3 @@
-local delete = function(state)
-	local inputs = require("neo-tree.ui.inputs")
-	local path = state.tree:get_node().path
-	local msg = "Are you sure you want to trash " .. path
-
-	inputs.confirm(msg, function(confirmed)
-		if not confirmed then
-			return
-		end
-
-		vim.fn.system({ "trash", vim.fn.fnameescape(path) })
-		require("neo-tree.sources.manager").refresh(state.name)
-	end)
-end
-
-local delete_visual = function(state, selected_nodes)
-	local inputs = require("neo-tree.ui.inputs")
-	local msg = "Are you sure you want to trash " .. #selected_nodes .. " files ?"
-
-	inputs.confirm(msg, function(confirmed)
-		if not confirmed then
-			return
-		end
-
-		for _, node in ipairs(selected_nodes) do
-			vim.fn.system({ "trash", vim.fn.fnameescape(node.path) })
-		end
-		require("neo-tree.sources.manager").refresh(state.name)
-	end)
-end
-
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	dependencies = {
@@ -94,8 +63,35 @@ return {
 				leave_dirs_open = false,
 			},
 			commands = {
-				delete = delete,
-				delete_visual = delete_visual,
+				delete = function(state)
+					local inputs = require("neo-tree.ui.inputs")
+					local path = state.tree:get_node().path
+					local msg = "Are you sure you want to trash " .. path
+
+					inputs.confirm(msg, function(confirmed)
+						if not confirmed then
+							return
+						end
+
+						vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+						require("neo-tree.sources.manager").refresh(state.name)
+					end)
+				end,
+				delete_visual = function(state, selected_nodes)
+					local inputs = require("neo-tree.ui.inputs")
+					local msg = "Are you sure you want to trash " .. #selected_nodes .. " files ?"
+
+					inputs.confirm(msg, function(confirmed)
+						if not confirmed then
+							return
+						end
+
+						for _, node in ipairs(selected_nodes) do
+							vim.fn.system({ "trash", vim.fn.fnameescape(node.path) })
+						end
+						require("neo-tree.sources.manager").refresh(state.name)
+					end)
+				end,
 			},
 		},
 	},
